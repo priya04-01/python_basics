976cc40df4acccacffeba6761df6844ffff8691f
priya <priyasingh15112001@gmail.com>
2025-01-18 18:19:11 +0530

    updated

--- mongodb/yt_mongodb.py
+++ mongodb/yt_mongodb.py
@@ -1,7 +1,15 @@
+from bson import ObjectId
 import pymongo
-client = pymongo.MongoClient("mongodb+srv://youtubepy:Pr7089819965@cluster0.meobw.mongodb.net/")
-db=client["ytmanager"]
+Client =pymongo.MongoClient("mongodb+srv://youtubepy:Pr7089819965@cluster0.meobw.mongodb.net/",ssl=True)
+db=Client["ytmanager"]
 video_collection=db["videos"]
+
+def list_videos():
+    for video in video_collection.find():
+        print(f"ID: {video['_id']}\n Name: {video['name']}, Time: {video['time']}")
+
+def update_video(name,n_name,n_time):
+    video_collection.update_one({"name":name},{"$set":{"name":n_name,"time":n_time}})
 
 def main():
     while True:
@@ -18,15 +26,17 @@ def main():
             time=input("Enter the video time: ")
             video_collection.insert_one({"name":name,"time":time})
         elif choice=='2':
+            list_videos()
             name=input("Enter the video name to delete:")
             video_collection.delete_one({"name":name})
         elif choice=='3':
+            list_videos()
             name=input("Enter the video name to update:")
-            time=input("Enter the new video time:")
-            video_collection.update_one({"name":name},{"$set":{"time":time}})
+            n_name=input("Enter the video name: ")
+            n_time=input("Enter the video time: ")
+            update_video(name,n_name,n_time)
         elif choice=='4':
-            for video in video_collection.find():
-                print(f"ID: {video['_id']}\n Name: {video['name']},Time: {video['time']}")
+          list_videos()
         elif choice=='5':
             break
         else:
